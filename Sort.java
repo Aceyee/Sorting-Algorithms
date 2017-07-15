f4d245e3d3d60d06192c10eaac52cf0845b4b359
Zihan Ye <zaeye1028@gmail.com>
2017-07-15 12:39:30 -0700

    Add a parent class to be extended

---
+++ Sort.java
@@ -0,0 +1,22 @@
+/* 
+   Sort.java :  A parent class that is extended by all child
+				classes. This class only contains a constructor
+				Sort() and a common method sort()
+
+   Author	: Zihan Ye
+   Date		: 2017.07.15
+   GitHub	: https://github.com/Aceyee
+   Email	: zaeye1028@gmail.com
+*/
+
+public class Sort{
+	public int []array;
+
+	public Sort(int []a){
+		array = a;
+	}
+
+	public void sort(){
+		// To be implemented in child classes
+	}
+}
