54ba193b18cb1bccd9c1defabe8a8f931c31dc3e
Zihan Ye <zaeye1028@gmail.com>
2017-07-16 05:24:31 -0700

    mergesort successful

--- MergeSort.java
+++ MergeSort.java
@@ -15,24 +15,62 @@ public class MergeSort extends Sort{
 	public void sort(){
 		mergesort(array, 0, array.length-1);
 	}
-	// 3 1 4     5 6 2
-	// 3 1  4
-	//
+
 	public void mergesort(int[] a, int low, int high){
-		int l = high-low+1;
-		//System.out.println(l);
-		if(l==1){
-			return;
-		}
-		int mid = 0;
-		if(l%2==0){
-			mid = l/2-1;
-		}else{
-			mid = l/2;
-		}
-		mergesort(a, 0, mid);
-		mergesort(a, mid+1, high);
+		if (low < high)
+        {
+            int mid = (low+high)/2;
+			mergesort(a, low, mid);
+			mergesort(a, mid+1, high);
 		
-		merge();
+			merge(a, low, mid ,high);
+		}
+	}
+
+	public int[] merge(int []a, int low, int mid, int high){ 
+		int l1 = mid-low+1;
+		int l2 = high-mid;
+		int []L = new int[l1];
+		int []R = new int[l2];
+		
+		for(int k=0; k<L.length; k++){
+			L[k] = a[low+k];
+		}
+
+		for(int k=0; k<R.length; k++){
+			R[k] = a[mid+k+1];
+		}
+
+		int i =0;
+		int j =0;
+		int index = low;
+
+		while(i<l1 && j<l2){
+			if(L[i]<R[j]){
+				a[index] = L[i];
+				index++;
+				i++;
+			}else{
+				a[index] = R[j];
+				index++;
+				j++;
+			}
+		}
+		while(i<l1){
+			a[index] = L[i];
+			index++;
+			i++;
+		}
+		while(j<l2){
+			a[index] = R[j];
+			index++;
+			j++;
+		}
+		/*
+		for(int k=0; k<a.length; k++){
+			System.out.print(a[k]+" ");
+		}
+		System.out.println();*/
+		return a;
 	}
 }
